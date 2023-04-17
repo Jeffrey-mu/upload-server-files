7ac0dab02d873f8341935d30083794a69f56a632
Jeffrey <1115378579@qq.com>
2023-04-17 14:54:31 +0800

    wip: 优化log

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@
 import { scp } from 'scp2'
 import ora from 'ora'
 import chalk from 'chalk'
+import log4js from 'log4js'
 interface Options {
   host: string
   username: string
   password: string
   path: string
 }
+const logger = log4js.getLogger()
+logger.level = 'debug'
 export default function dep(localPath: string, options: Options) {
   const start = +new Date()
   const spinner = ora(chalk.green('正在发布到服务器...'))
   spinner.start()
   if (!localPath) {
-    log('grey', '本地路径不可为空！')
+    logger.info('本地路径不可为空！')
     return
   }
   scp(
@@ -22,57 +25,12 @@ export default function dep(localPath: string, options: Options) {
     (err?: Error) => {
       spinner.stop()
       if (!err) {
-        log('yellow', `${+new Date() - start}ms`)
-        log('green', '项目发布完毕')
+        logger.warn(`${+new Date() - start}ms`)
+        logger.info('项目发布完毕')
       }
       else {
-        log('red', '发布失败！')
+        logger.error('发布失败！')
       }
     },
   )
 }
-
-function log(color: Color, text: string) {
-  const spinner = ora(chalk[color](text))
-  spinner.start()
-  spinner.stop()
-}
-
-type Color =
-  'visible'
-  | 'black'
-  | 'red'
-  | 'green'
-  | 'yellow'
-  | 'blue'
-  | 'magenta'
-  | 'cyan'
-  | 'white'
-  | 'gray'
-  | 'grey'
-  | 'blackBright'
-  | 'redBright'
-  | 'greenBright'
-  | 'yellowBright'
-  | 'blueBright'
-  | 'magentaBright'
-  | 'cyanBright'
-  | 'whiteBright'
-  | 'bgBlack'
-  | 'bgRed'
-  | 'bgGreen'
-  | 'bgYellow'
-  | 'bgBlue'
-  | 'bgMagenta'
-  | 'bgCyan'
-  | 'bgWhite'
-  | 'bgGray'
-  | 'bgGrey'
-  | 'bgBlackBright'
-  | 'bgRedBright'
-  | 'bgGreenBright'
-  | 'bgYellowBright'
-  | 'bgBlueBright'
-  | 'bgMagentaBright'
-  | 'bgCyanBright'
-  | 'bgWhiteBright'
