872a10c814899636fde2fe1337c1ff7897c15501
Jeffrey <1115378579@qq.com>
2023-04-17 12:09:39 +0800

    feat: 优化打印信息样式

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export default function dep(localPath: string, options: Options) {
   const spinner = ora(chalk.green('正在发布到服务器...'))
   spinner.start()
   if (!localPath) {
-    ora(chalk.green('本地路径不可为空！'))
+    log('grey', '本地路径不可为空！')
     return
   }
   scp(
@@ -22,12 +22,57 @@ export default function dep(localPath: string, options: Options) {
     (err?: Error) => {
       spinner.stop()
       if (!err) {
-        ora(`${+new Date() - start}ms`)
-        ora(chalk.green('项目发布完毕!'))
+        log('yellow', `${+new Date() - start}ms`)
+        log('green', '项目发布完毕')
       }
       else {
-        ora(chalk.red('发布失败！'))
+        log('red', '发布失败！')
       }
     },
   )
 }
+
+function log(color: Color, text: string) {
+  const spinner = ora(chalk[color](text))
+  spinner.start()
+  spinner.stop()
+}
+
+type Color =
+  'visible'
+  | 'black'
+  | 'red'
+  | 'green'
+  | 'yellow'
+  | 'blue'
+  | 'magenta'
+  | 'cyan'
+  | 'white'
+  | 'gray'
+  | 'grey'
+  | 'blackBright'
+  | 'redBright'
+  | 'greenBright'
+  | 'yellowBright'
+  | 'blueBright'
+  | 'magentaBright'
+  | 'cyanBright'
+  | 'whiteBright'
+  | 'bgBlack'
+  | 'bgRed'
+  | 'bgGreen'
+  | 'bgYellow'
+  | 'bgBlue'
+  | 'bgMagenta'
+  | 'bgCyan'
+  | 'bgWhite'
+  | 'bgGray'
+  | 'bgGrey'
+  | 'bgBlackBright'
+  | 'bgRedBright'
+  | 'bgGreenBright'
+  | 'bgYellowBright'
+  | 'bgBlueBright'
+  | 'bgMagentaBright'
+  | 'bgCyanBright'
+  | 'bgWhiteBright'
