ef6fb3f6b2cb20a1d5df7be19cef94bb0f002b1b
Jeffrey <1115378579@qq.com>
2023-04-17 11:40:30 +0800

    feat: 添加服务端推送文件

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-export const one = 1
-export const two = 2
+'use strict'
+import client from 'scp2'
+import ora from 'ora'
+import chalk from 'chalk'
+interface Options {
+  host: string
+  username: string
+  password: string
+  path: string
+}
+export default function dep(localPath: string, options: Options) {
+  const start = +new Date()
+  const spinner = ora(chalk.green('正在发布到服务器...'))
+  spinner.start()
+  // @ts-expect-error
+  if (!localPath) {
+    console.log(chalk.green('本地路径不可为空！'))
+    return
+  }
+  client.scp(
+    localPath,
+    options,
+    (err: string) => {
+      spinner.stop()
+      if (!err) {
+        console.log(`${+new Date() - start}ms`)
+        console.log(chalk.green('项目发布完毕!'))
+      }
+      else {
+        console.log(chalk.red('发布失败！'), err)
+      }
+    },
+  )
+}
---
+++ src/scp2.d.ts
@@ -0,0 +1,59 @@
+declare module 'scp2' {
+  import type { Client, ConnectConfig } from 'ssh2'
+
+  interface Options extends ConnectConfig {
+    file?: FileSpec
+    path?: string
+    watch?: boolean
+    confirm?: boolean
+    recursive?: boolean
+    concurrency?: number
+    tick?: (localPath: string, remotePath: string, error: Error | null) => void
+    step?: (totalTransferred: number, chunk: number, total: number) => void
+    privateKey?: string | Buffer
+    passphrase?: string
+    agentForward?: boolean
+    agent?: string
+    agentCommand?: string
+    agentForwardIdentities?: boolean
+    dstPort?: number
+    srcPort?: number
+    sshConfig?: any
+    strictVendor?: boolean
+    algorithms?: Algorithms
+  }
+
+  interface FileSpec {
+    source: string
+    destination: string
+  }
+
+  interface Algorithms {
+    kex?: string[]
+    cipher?: string[]
+    serverHostKey?: string[]
+    hmac?: string[]
+  }
+
+  function scp(
+    src: string,
+    dest: string,
+    opts: Options,
+    cb: (err?: Error) => void
+  ): void
+
+  function scp(
+    files: FileSpec[],
+    opts: Options,
+    cb: (err?: Error) => void
+  ): void
+
+  class ClientWrapper {
+    client: Client
+    scp: typeof scp // 显式声明 scp 方法
+    constructor(config: ConnectConfig)
+    close(): void
+  }
+
+  export default function createClient(opt: ConnectConfig): Promise<ClientWrapper>
+}
